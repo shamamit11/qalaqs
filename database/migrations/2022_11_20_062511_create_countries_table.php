c980e72a4d10c3ebfcb19ce22f0cbf3215ddcccf
unknown <udeepstha@gmail.com>
2022-11-20 16:01:40 +0545

    login

--- database/migrations/2022_11_20_062511_create_countries_table.php
+++ database/migrations/2022_11_20_062511_create_countries_table.php
@@ -15,9 +15,8 @@ return new class extends Migration
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
-            $table->string('code')->nullable();
             $table->string('name')->nullable();
-            $table->timestamps();
+            $table->string('code')->nullable();
         });
     }
 
