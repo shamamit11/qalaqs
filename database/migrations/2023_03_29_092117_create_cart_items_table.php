fc95c637debe870485440c64ad7e4a54d84c32a6
Amit Sharma <shamamit11@gmail.com>
2023-03-29 13:25:51 +0400

    create db tables

---
+++ database/migrations/2023_03_29_092117_create_cart_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cart_id');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('item_count');
+            $table->decimal('sub_total', 12, 2)->nullable();
+            $table->decimal('tax_total', 12, 2)->nullable();
+            $table->decimal('delivery_charge', 12, 2)->nullable();
+            $table->decimal('grand_total', 12, 2)->nullable();
+            $table->unsignedBigInteger('vendor_id');
+            $table->foreign('cart_id')->references('id')->on('carts')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreign('product_id')->references('id')->on('products')->cascadeOnUpdate();
+            $table->foreign('vendor_id')->references('id')->on('vendors')->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
