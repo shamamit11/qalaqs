928b9f52f30ae1ba50daaaeb1ba2a08d6462c446
Amit Sharma <shamamit11@gmail.com>
2023-08-29 20:32:10 +0400

    fix

--- app/Services/Api/User/ProductService.php
+++ app/Services/Api/User/ProductService.php
@@ -69,8 +69,8 @@ class ProductService
 
         try {
             $conditions = [['status', '1'], ['admin_approved', '1'], ['part_type', 'New']];
-            //$products = Product::where($conditions)->orderBy('id', 'desc')->paginate($perPage);
-            $products = Product::where($conditions)->orderBy('id', 'desc')->take($limit)->get();
+            $products = Product::where($conditions)->orderBy('id', 'desc')->paginate($limit);
+            //$products = Product::where($conditions)->orderBy('id', 'desc')->take($limit)->get();
 
             if ($products->count() > 0) {
                 foreach ($products as $product) {
