27bbd9b4fd39b9819e06f48de0b35a068acbb918
Amit Sharma <shamamit11@gmail.com>
2023-09-11 22:41:38 +0400

    fix

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -6,19 +6,19 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-xl-3 col-md-6">
-                        <div class="card">
+                        <div class="card text-white bg-info">
                             <div class="card-body">
 
-                                <h4 class="header-title mt-0 mb-3">Special Requests</h4>
+                                <h4 class="header-title text-white mt-0 mb-3">Special Requests</h4>
 
                                 <div class="widget-box-2">
                                     <div class="widget-detail-2 text-start float-start">
-                                        <h2 class="fw-normal mb-1"> {{ $today_special_requests }} </h2>
-                                        <p class="text-muted mb-3">Today's Requests</p>
+                                        <h2 class="fw-normal text-white mb-1"> {{ $today_special_requests }} </h2>
+                                        <p class="text-white mb-3">Today's Requests</p>
                                     </div>
                                     <div class="widget-detail-2 text-end float-end">
-                                        <h2 class="fw-normal mb-1"> {{ $total_special_requests }} </h2>
-                                        <p class="text-muted mb-3">Total Requests</p>
+                                        <h2 class="fw-normal text-white mb-1"> {{ $total_special_requests }} </h2>
+                                        <p class="mb-3">Total Requests</p>
                                     </div>
                                     <div class="clearfix"></div>
                                 </div>
@@ -28,19 +28,19 @@
                     </div>
 
                     <div class="col-xl-3 col-md-6">
-                        <div class="card">
+                        <div class="card text-white bg-danger">
                             <div class="card-body">
 
-                                <h4 class="header-title mt-0 mb-3">Sales / Orders</h4>
+                                <h4 class="header-title text-white mt-0 mb-3">Sales / Orders</h4>
 
                                 <div class="widget-box-2">
                                     <div class="widget-detail-2 text-start float-start">
-                                        <h2 class="fw-normal mb-1"> {{ $today_orders }} </h2>
-                                        <p class="text-muted mb-3">Today's Orders</p>
+                                        <h2 class="fw-normal text-white mb-1"> {{ $today_orders }} </h2>
+                                        <p class="mb-3 text-white">Today's Orders</p>
                                     </div>
                                     <div class="widget-detail-2 text-end float-end">
-                                        <h2 class="fw-normal mb-1"> {{ $total_orders }} </h2>
-                                        <p class="text-muted mb-3">Total Orders</p>
+                                        <h2 class="fw-normal text-white mb-1"> {{ $total_orders }} </h2>
+                                        <p class="mb-3 text-white">Total Orders</p>
                                     </div>
                                     <div class="clearfix"></div>
                                 </div>
@@ -51,19 +51,19 @@
 
 
                     <div class="col-xl-3 col-md-6">
-                        <div class="card">
+                        <div class="card text-white bg-purple">
                             <div class="card-body">
 
-                                <h4 class="header-title mt-0 mb-3">Returns / Exchanges</h4>
+                                <h4 class="header-title text-white mt-0 mb-3">Returns / Exchanges</h4>
 
                                 <div class="widget-box-2">
                                     <div class="widget-detail-2 text-start float-start">
-                                        <h2 class="fw-normal mb-1"> {{ $today_returns }} </h2>
-                                        <p class="text-muted mb-3">Today's Returns</p>
+                                        <h2 class="fw-normal text-white mb-1"> {{ $today_returns }} </h2>
+                                        <p class="mb-3 text-white">Today's Returns</p>
                                     </div>
                                     <div class="widget-detail-2 text-end float-end">
-                                        <h2 class="fw-normal mb-1"> {{ $total_returns }} </h2>
-                                        <p class="text-muted mb-3">Total Returns</p>
+                                        <h2 class="fw-normal text-white mb-1"> {{ $total_returns }} </h2>
+                                        <p class="mb-3 text-white">Total Returns</p>
                                     </div>
                                     <div class="clearfix"></div>
                                 </div>
@@ -75,19 +75,19 @@
 
 
                     <div class="col-xl-3 col-md-6">
-                        <div class="card">
+                        <div class="card text-white bg-pink">
                             <div class="card-body">
 
-                                <h4 class="header-title mt-0 mb-3">Revenue</h4>
+                                <h4 class="header-title text-white mt-0 mb-3">Revenue</h4>
 
                                 <div class="widget-box-2">
                                     <div class="widget-detail-2 text-start float-start">
-                                        <h2 class="fw-normal mb-1"> {{ $today_sales }} </h2>
-                                        <p class="text-muted mb-3">Today Sales</p>
+                                        <h2 class="fw-normal text-white mb-1"> {{ $today_sales }} </h2>
+                                        <p class="mb-3">Today Sales</p>
                                     </div>
                                     <div class="widget-detail-2 text-end float-end">
-                                        <h2 class="fw-normal mb-1"> {{ $total_sales }} </h2>
-                                        <p class="text-muted mb-3">Total Sales</p>
+                                        <h2 class="fw-normal text-white mb-1"> {{ $total_sales }} </h2>
+                                        <p class="mb-3 text-white">Total Sales</p>
                                     </div>
                                     <div class="clearfix"></div>
                                 </div>
@@ -100,11 +100,11 @@
 
                 <div class="row">
                     <div class="col-xl-3 col-md-6">
-                        <div class="card">
+                        <div class="card text-white bg-pink">
                             <div class="card-body widget-user">
                                 <div class="text-center">
-                                    <h2 class="fw-normal text-primary" data-plugin="counterup"> {{ $vendors_garage }}</h2>
-                                    <h5>Today's Vendor Registrations - Garage</h5>
+                                    <h2 class="fw-normal text-white" data-plugin="counterup"> {{ $vendors_garage }}</h2>
+                                    <h5 class="text-white">Today's Vendor Registrations - Garage</h5>
                                 </div>
                             </div>
                         </div>
@@ -112,11 +112,11 @@
                     </div>
 
                     <div class="col-xl-3 col-md-6">
-                        <div class="card">
+                        <div class="card text-white bg-purple">
                             <div class="card-body widget-user">
                                 <div class="text-center">
-                                    <h2 class="fw-normal text-primary" data-plugin="counterup"> {{ $vendors_seller }}</h2>
-                                    <h5>Today's Vendor Registrations - Seller</h5>
+                                    <h2 class="fw-normal text-white" data-plugin="counterup"> {{ $vendors_seller }}</h2>
+                                    <h5 class="text-white">Today's Vendor Registrations - Seller</h5>
                                 </div>
                             </div>
                         </div>
@@ -124,12 +124,12 @@
                     </div>
 
                     <div class="col-xl-3 col-md-6">
-                        <div class="card">
+                        <div class="card text-white bg-danger">
                             <div class="card-body widget-user">
                                 <div class="text-center">
-                                    <h2 class="fw-normal text-primary" data-plugin="counterup"> {{ $approved_vendors }}
+                                    <h2 class="fw-normal text-white" data-plugin="counterup"> {{ $approved_vendors }}
                                     </h2>
-                                    <h5>Total Approved Vendors</h5>
+                                    <h5 class="text-white">Total Approved Vendors</h5>
                                 </div>
                             </div>
                         </div>
@@ -137,11 +137,11 @@
                     </div>
 
                     <div class="col-xl-3 col-md-6">
-                        <div class="card">
+                        <div class="card text-white bg-info">
                             <div class="card-body widget-user">
                                 <div class="text-center">
-                                    <h2 class="fw-normal text-primary" data-plugin="counterup"> {{ $pending_vendors }}</h2>
-                                    <h5>Total Pending Vendors</h5>
+                                    <h2 class="fw-normal text-white" data-plugin="counterup"> {{ $pending_vendors }}</h2>
+                                    <h5 class="text-white">Total Pending Vendors</h5>
                                 </div>
                             </div>
                         </div>
@@ -152,11 +152,11 @@
 
                 <div class="row">
                     <div class="col-xl-3 col-md-6">
-                        <div class="card">
+                        <div class="card text-white bg-info">
                             <div class="card-body widget-user">
                                 <div class="text-center">
-                                    <h2 class="fw-normal text-primary" data-plugin="counterup"> {{ $user_individual }}</h2>
-                                    <h5>Today's User Registrations - Individual</h5>
+                                    <h2 class="fw-normal text-white" data-plugin="counterup"> {{ $user_individual }}</h2>
+                                    <h5 class="text-white">Today's User Registrations - Individual</h5>
                                 </div>
                             </div>
                         </div>
@@ -164,11 +164,11 @@
                     </div>
 
                     <div class="col-xl-3 col-md-6">
-                        <div class="card">
+                        <div class="card text-white bg-danger">
                             <div class="card-body widget-user">
                                 <div class="text-center">
-                                    <h2 class="fw-normal text-primary" data-plugin="counterup"> {{ $user_garage }}</h2>
-                                    <h5>Today's User Registrations - Garage</h5>
+                                    <h2 class="fw-normal text-white" data-plugin="counterup"> {{ $user_garage }}</h2>
+                                    <h5 class="text-white">Today's User Registrations - Garage</h5>
                                 </div>
                             </div>
                         </div>
@@ -176,12 +176,12 @@
                     </div>
 
                     <div class="col-xl-3 col-md-6">
-                        <div class="card">
+                        <div class="card text-white bg-purple">
                             <div class="card-body widget-user">
                                 <div class="text-center">
-                                    <h2 class="fw-normal text-primary" data-plugin="counterup"> {{ $total_user_individual }}
+                                    <h2 class="fw-normal text-white" data-plugin="counterup"> {{ $total_user_individual }}
                                     </h2>
-                                    <h5>Total Users - Individuals</h5>
+                                    <h5 class="text-white">Total Users - Individuals</h5>
                                 </div>
                             </div>
                         </div>
@@ -189,12 +189,12 @@
                     </div>
 
                     <div class="col-xl-3 col-md-6">
-                        <div class="card">
+                        <div class="card text-white bg-pink">
                             <div class="card-body widget-user">
                                 <div class="text-center">
-                                    <h2 class="fw-normal text-primary" data-plugin="counterup"> {{ $total_user_garage }}
+                                    <h2 class="fw-normal text-white" data-plugin="counterup"> {{ $total_user_garage }}
                                     </h2>
-                                    <h5>Total Users - Garage</h5>
+                                    <h5 class="text-white">Total Users - Garage</h5>
                                 </div>
                             </div>
                         </div>
@@ -206,7 +206,7 @@
                 <div class="row">
                     <div class="col-xl-4 col-md-6">
                         <div class="card">
-                            <h4 class="card-header">Top Selling Parts</h4>
+                            <h4 class="card-header bg-dark text-white">Top Selling Parts</h4>
                             <div class="card-body">
                                 @foreach($topSellingProducts as $tsp)
                                     <h5 class="mt-0">{{ $tsp->title }} <span class="text-primary float-end">{{$tsp->total_sales}}</span></h5>
@@ -218,7 +218,7 @@
 
                     <div class="col-xl-4 col-md-6">
                         <div class="card">
-                            <h4 class="card-header">Top Selling Vendors</h4>
+                            <h4 class="card-header bg-dark text-white">Top Selling Vendors</h4>
                             <div class="card-body">
                                 @foreach($topSellingVendors as $tsv)
                                     <h5 class="mt-0">{{ $tsv->business_name }} <span class="text-primary float-end">{{$tsv->total_sales}}</span></h5>
@@ -230,7 +230,7 @@
 
                     <div class="col-xl-4 col-md-6">
                         <div class="card">
-                            <h4 class="card-header">Top Selling Makes</h4>
+                            <h4 class="card-header bg-dark text-white">Top Selling Makes</h4>
                             <div class="card-body">
                                 @foreach($topSellingProducts as $tsp)
                                     <h5 class="mt-0">{{ $tsp->make }} <span class="text-primary float-end">{{$tsv->total_sales}}</span></h5>
